5c74063ba921364b7ab4b83e3bd52fd3d6ad8ff9
marcelo <duemarfra@gmail.com>
2024-07-04 23:51:25 -0300

    optimizando codigo de skils

--- utils/js/main.js
+++ utils/js/main.js
@@ -1,4 +1,24 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+// utils/js/main.js
+document.addEventListener('DOMContentLoaded', () => {
+  const renderTechnologies = async () => {
+    const response = await fetch('../developer/utils/json/technologies.json');
+    const technologies = await response.json();
+
+    const technologiesList = document.getElementById('technologiesList');
+    technologiesList.innerHTML = technologies.map(technologie => `
+      <li>
+        <a target="new" href="${technologie.url}" data-bs-toggle="tooltip" data-bs-html="true" title="<u>${technologie.tooltip}</u>">
+          <img src="${technologie.imgSrc}" alt="${technologie.alt}">
+          <br>${technologie.name}
+        </a>
+      </li>
+    `).join('');
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  };
+
+  renderTechnologies();
+});
