f62fa8bc225f1fa27f198a15721c7ff0fdbf4111
marcelo <duemarfra@gmail.com>
2024-07-05 02:12:14 -0300

    optimizadas las secciones

--- utils/js/main.js
+++ utils/js/main.js
@@ -1,5 +1,5 @@
-// utils/js/main.js
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+
   const renderTechnologies = async () => {
     const response = await fetch('../developer/utils/json/technologies.json');
     const technologies = await response.json();
@@ -38,13 +38,46 @@ document.addEventListener('DOMContentLoaded', () => {
         </div>
       </div>
     `).join('');
-
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
   };
 
+  const renderCertificates = async () => {
+    const response = await fetch('../developer/utils/json/certificates.json');
+    const certificates = await response.json();
+
+    const certificatesList = document.getElementById('certificatesList');
+    certificatesList.innerHTML = certificates.map(certificate => `
+      <div class="col-10 offset-1 col-md-4 offset-md-0 col-sm-6 offset-sm-0 bg-danger p-0 targetaPortafolio">
+        <figure>
+          <img src="${certificate.src}" class="img-fluid" alt="laptop">
+          <div class="text-white capaPortafolio">
+            <h5 class="text-uppercase">${certificate.title}</h5>
+            <hr>
+            <!-- <a href="https://xn--marcelo-dueas-skb.com/" target="new"><i class="bi bi-globe"></i></a>
+            <a href="https://github.com/duemarfra/portafolioBootrap.git" target="new"><i class="bi bi-github"></i></a> -->
+          </div>
+        </figure>
+      </div>
+
+    `).join('');
+  };
+
+  const renderContactos = async () => {
+    const response = await fetch('../developer/utils/json/contactos.json');
+    const contactos = await response.json();
+
+    const contactosLists = document.querySelectorAll('.contactosList');
+
+    contactosLists.forEach(contactosList => {
+      contactosList.innerHTML = contactos.map(contacto => `
+          <li>
+            <a href="${contacto.href}" target="new" class="${contacto.class}"><i class="${contacto.iclass}"></i></a>
+          </li>
+        `).join('');
+    });
+  };
+
+  renderContactos();
   renderTechnologies();
   renderServices();
+  renderCertificates();
 });
