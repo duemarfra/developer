5863cc7d4fd053659b88b7085b4c9fc964708a52
marcelo <duemarfra@gmail.com>
2024-07-05 00:19:40 -0300

    optimizado codigo de servicios

--- utils/js/main.js
+++ utils/js/main.js
@@ -20,5 +20,31 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   };
 
+  const renderServices = async () => {
+    const response = await fetch('../developer/utils/json/services.json');
+    const services = await response.json();
+
+    const servicesList = document.getElementById('servicesList');
+    servicesList.innerHTML = services.map(service => `
+      <div class="col-10 offset-1 col-md-4 offset-md-0 col-sm-6 offset-sm-0">
+        <div class="card text-dark bg-primary mb-3">
+          <div class="card-body text-light tarjetaEstilo">
+            <div class="text-center">
+              <h4><i class="${service.icon}"></i></h4>
+              <h5 class="card-title">${service.title}</h5>
+            </div>
+            <p class="card-text textoJustificado">${service.body}</p>
+          </div>
+        </div>
+      </div>
+    `).join('');
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  };
+
   renderTechnologies();
+  renderServices();
 });
